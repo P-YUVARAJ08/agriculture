bac8f5c7f93acb327a884a0cda39dde2c2ac362c
P-YUVARAJ08 <148528605+P-YUVARAJ08@users.noreply.github.com>
2023-11-05 18:21:33 -0800

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.boxs')
+    const menuList = document.querySelector('nav ul')
+    menu.addEventListener('click',()=>{
+menuList.classList.toggle('showmenu')
+    })
